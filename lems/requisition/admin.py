0cade1c80bd2ebaed83b48d9ba5c2c705d36fce7
Bikesh <bikeshmaharjan@gmail.com>
2022-07-16 12:22:31 +0545

    Requisition models added

--- lems/requisition/admin.py
+++ lems/requisition/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Requisition
+from .models import Requisition_compo
 
-# Register your models here.
+
+@admin.register(Requisition)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['user','total_compo']
+
+@admin.register(Requisition_compo)
+class ComponentAdmin(admin.ModelAdmin):
+    list_display = ['user','component','quantity','requisition']
+
--- lems/requisition/models.py
+++ lems/requisition/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from user.models import UserProfile
 
-# Create your models here.
+class Requisition(models.Model):
+    user= models.ForeignKey(UserProfile,on_delete=models.CASCADE,null=True, default= "0" , blank=True)
+    total_compo = models.IntegerField()
+
+class Requisition_compo(models.Model):
+    requisition= models.ForeignKey(Requisition,on_delete=models.CASCADE,null=True, default= "0" , blank=True)
+    user= models.ForeignKey(UserProfile,on_delete=models.CASCADE,null=True, default= "0" , blank=True)
+    component= models.CharField(max_length=20)
+    quantity = models.IntegerField()
+
+ 
